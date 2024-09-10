e91b6b0513030ed79ea72daf98455291a83c62c4
Kevyn Ramirez <kevynramirez@Kevyns-Laptop.local>
2024-09-09 19:54:07 -0700

    adding svg icons for leftside broke the page, now it's all white and there appears to be nothing in the body when inspecting element

--- react-website/src/LeftSide.jsx
+++ react-website/src/LeftSide.jsx
@@ -1,12 +1,27 @@
 import './LeftSide.css'
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/fontawesome-svg-core'
+
 
 function LeftSide () {
     return (
         <div className='left-half'>
-            
-            <h1>Kevyn Antonio Ramirez</h1>
-            <sub>Developer</sub>
-            <p>I am honing my skills by designing and implementing projects I find interesting.</p>
+            <div className='left-half-section-one'>
+                <h1>Kevyn Antonio Ramirez</h1>
+                <sub>Developer</sub>
+                <p>I am honing my skills by designing and implementing projects I find interesting, starting with Frontend Development!</p>
+            </div>
+
+            <ul>
+                <li><a href='#aboutme'>About Me</a></li>
+                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+
+            <ul>
+                <li><a href='https://www.linkedin.com/in/kevyn-ramirez/'><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            </ul>
         </div>
     );
 };
